46e7dbef5b88036acb3ef0d4fba1d2df5b0e04c2
elyasahmadi79 <elyas.ahmady1@gmail.com>
2023-03-30 11:49:38 +0430

    Picture  added

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
       MaterialApp(
+        debugShowCheckedModeBanner: false,
       home: Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
         title: Center(child: Text('LalehStudio.af')),
         backgroundColor: Colors.deepPurple,
+      ),
+        body: Center(
+          child: Image(image: AssetImage('images/Poem2.jpg')),
       )
       ),
+  ),
   );
 }
